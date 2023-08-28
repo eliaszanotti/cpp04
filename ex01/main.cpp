0620306e1427e48c8ac505606290d4e35fd5bf34
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-08-28 16:35:14 +0200

    del wrong class in ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: elias <elias@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 14:54:28 by elias             #+#    #+#             */
-/*   Updated: 2023/08/28 12:47:31 by elias            ###   ########.fr       */
+/*   Updated: 2023/08/28 16:34:40 by elias            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 #include "Animal.class.hpp"
 #include "Cat.class.hpp"
 #include "Dog.class.hpp"
-#include "Wrong.class.hpp"
 #include "Brain.class.hpp"
 
 int main(void)
