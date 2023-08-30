3540f401218001617cd3325ec7794c3a27fa9f62
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-08-30 14:57:12 +0200

    remove wrong class in ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: elias <elias@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 14:54:28 by elias             #+#    #+#             */
-/*   Updated: 2023/08/28 12:54:49 by elias            ###   ########.fr       */
+/*   Updated: 2023/08/30 14:56:49 by elias            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 #include "Animal.class.hpp"
 #include "Cat.class.hpp"
 #include "Dog.class.hpp"
-#include "Wrong.class.hpp"
 #include "Brain.class.hpp"
 #include "AAnimal.class.hpp"
 
